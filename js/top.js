b3114bcd1b0d540a3a898684d13c33d85a8fb103
伊藤京子 <okano@u-d.jp>
2019-12-26 11:41:47 +0900

    スタイル修正、モーダル復活

--- js/top.js
+++ js/top.js
@@ -20,14 +20,93 @@ $(".iopt_radio_label").each(function (i) {
 
 });
 
+//ラベルにID振り分け
+$("label").each(function (i) {
+    $(this).attr('id', 'label' + (i + 1));
+});
+
+
+/*
+ツールチップ
+------------------------------------------*/
+
+/*バナーサイズs*/
+
+//Sサイズ選択
+tippy('#size__select > #label2', {
+    content: '長辺と短編の合計が600px未満　3600円',
+    interactive: true
+});
+
+//Mサイズ選択
+tippy('#size__select > #label3', {
+    content: '長辺と短編の合計が601px～1000px　5700円',
+    interactive: true
+});
+
+//Lサイズ選択
+tippy('#size__select > #label4', {
+    content: '長辺と短編の合計が1001px～1500px　8600円',
+    interactive: true
+});
+
+//SNS用ヘッダー/ブログ用ヘッダー
+tippy('#size__select > #label5', {
+    content: '11500円',
+    interactive: true
+});
+
+//リサイズ依頼
+tippy('#size__select > #label6', {
+    content: '上記価格の50%OFFの金額',
+    interactive: true
+});
+
+//その他
+tippy('#size__select > #label7', {
+    content: '特殊サイズのバナーです。サイズをご指定ください',
+    interactive: true
+});
+
+/*バナー形式*/
+//その他
+tippy('#format > #label15', {
+    content: '+2000円',
+    interactive: true
+});
+
+/*納期選択*/
+tippy('#limit > #label17', {
+    content: '+2000円',
+    interactive: true
+});
+
+tippy('#limit > #label18', {
+    content: '+1000円',
+    interactive: true
+});
+
+/*データ選択*/
+
+tippy('#data > #label23', {
+    content: '+1500円',
+    interactive: true
+});
+
+/*実績として公開*/
+
+tippy('#publish > #label25', {
+    content: '-500円',
+    interactive: true
+});
 
 
 $(function () {
 
-    $("#size__select label").change(function () {
+    $(".select__zone label").change(function () {
 
         //選択したボックスにスタイリング
-        $("#size__select label").not(this).removeClass("checked");
+        $(".select__zone label").not(this).removeClass("checked");
 
         $(this).addClass("checked");
 
@@ -44,7 +123,7 @@ $(function () {
         }
 
         //特殊サイズを選択
-        if ($('[value="%E7%89%B9%E6%AE%8A%E3%82%B5%E3%82%A4%E3%82%BA%E3%83%90%E3%83%8A%E3%83%BC"]').prop('checked')) {
+        if ($('[value="%E3%81%9D%E3%81%AE%E4%BB%96"]').prop('checked')) {
             $('#control__num').css("display", "none");
             $('#special__size').css("display", "block");
         }
@@ -103,7 +182,7 @@ $(function () {
 
     //オプション選択のラベルテキスト取得TABLEへ出力
 
-    $('#option >label').change(function () {
+    $('#limit >label').change(function () {
 
         var label01 = $(this).text();
 
@@ -150,8 +229,18 @@ $(function () {
     //モーダルないのボタンクリックしたら管理番号入力
     btn.on("click", function () {
         var value = $(this).val();
-        $(".text2").val(value);
-        $(".bnr__modal__box").fadeOut();
-        $("#modal02").fadeIn();
+        var pos = $("#size__select").offset().top;
+        $("#control__num").css("display", "block");
+        $("html,body").animate({ scrollTop: pos }, 'fast');
+        $(".text1").val(value);
+        return false;
+
     });
+});
+
+
+//無限スクロール表示設定
+
+$(function () {
+    $('#infinite__banners').jscroll();
 });
